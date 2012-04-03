e1037d87aeb64632051897a92e3898277f3219e8
Jukka Svahn <web@rahinaa.biz>
2012-04-03 22:49:46 +0300

    Added installer/uninstaller, use plugin-lifecycle. Wrapped to a class.

--- rah_custom_feed.php
+++ rah_custom_feed.php
@@ -8,25 +8,50 @@
  * @license GNU GPLv2
  * @link http://rahforum.biz/plugins/rah_custom_feed
  *
+ * Requires Textpattern v4.2.0 or newer.
+ *
  * Copyright (C) 2011 Jukka Svahn <http://rahforum.biz>
  * Licensed under GNU Genral Public License version 2
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-	register_callback('rah_custom_feed','atom_entry');
-	register_callback('rah_custom_feed','rss_entry');
+	register_callback(array('rah_custom_feed', 'install'), 'plugin_lifecycle.rah_custom_feed');
+	register_callback(array('rah_custom_feed', 'feed_item'), 'atom_entry');
+	register_callback(array('rah_custom_feed', 'feed_item'), 'rss_entry');
 
-	if (@txpinterface == 'admin') {
-		if(gps('event') == 'form' && safe_count('txp_form',"name='rah_feed_body'") == 0) {
-			safe_insert('txp_form',"name='rah_feed_body', type='article', Form='<txp:body />'");
+class rah_custom_feed {
+	
+	/**
+	 * Installer
+	 * @param string $event Admin-side event.
+	 * @param string $step Admin-side, plugin-lifecycle step.
+	 */
+	
+	static public function install($event='', $step='') {
+		
+		if($step == 'deleted') {
+			
+			safe_delete(
+				'txp_form',
+				"name='rah_feed_body'"
+			);
+			
+			return;
+		}
+		
+		if(!safe_row('name', 'txp_form', "name='rah_feed_body'")) {
+			safe_insert(
+				'txp_form',
+				"name='rah_feed_body', type='article', Form='<txp:body />'"
+			);
 		}
 	}
 
-/**
- * Modifies feeds excerpt and body content
- */
+	/**
+	 * Modifies feeds excerpt and body content
+	 */
 
-	function rah_custom_feed() {
+	static public function feed_item() {
 		global $thisarticle;
 		@$form = fetch_form('rah_feed_body');
 		
@@ -36,4 +61,6 @@
 			$thisarticle['body'] = $form;
 		}
 	}
+}
+
 ?>
