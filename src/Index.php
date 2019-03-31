dd8dbc8f9cadaa9518a0d330fdd1a06a39b11a6e
Jukka Svahn <void@rah.pw>
2019-03-31 14:59:06 +0300

    Refactor

---
+++ src/Index.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * rah_custom_feed - Customized RSS and Atom feeds for Textpattern CMS
+ * https://github.com/gocom/rah_custom_feed
+ *
+ * Copyright (C) 2019 Jukka Svahn
+ *
+ * This file is part of rah_custom_feed.
+ *
+ * rah_custom_feed is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * rah_custom_feed is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rah_custom_feed. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+new Rah_Custom_Feed();
---
+++ src/Rah/Custom/Feed.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * rah_custom_feed - Customized RSS and Atom feeds for Textpattern CMS
+ * https://github.com/gocom/rah_custom_feed
+ *
+ * Copyright (C) 2019 Jukka Svahn
+ *
+ * This file is part of rah_custom_feed.
+ *
+ * rah_custom_feed is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * rah_custom_feed is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rah_custom_feed. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Plugin class.
+ */
+final class Rah_Custom_Feed
+{
+    /**
+     * Constructor.
+     */
+    public function __construct(): void
+    {
+        register_callback([$this, 'install'], 'plugin_lifecycle.rah_custom_feed', 'installed');
+        register_callback([$this, 'item'], 'atom_entry');
+        register_callback([$this, 'item'], 'rss_entry');
+    }
+
+    /**
+     * Installer.
+     */
+    public function install(): void
+    {
+        create_form('rah_feed_body', 'misc', '');
+    }
+
+    /**
+     * Invoked on every feed item.
+     */
+    public function item(): void
+    {
+        global $thisarticle;
+
+        if (fetch_form('rah_feed_body') === false) {
+            return;
+        }
+
+        $form = parse_form('rah_feed_body');
+        $thisarticle['excerpt'] = $form;
+        $thisarticle['body'] = $form;
+    }
+}
